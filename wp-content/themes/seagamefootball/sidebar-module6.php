5632696d7a32b357eae47b62f2fc75a216ccbe75
doanvuquockhiem <Doanvukhiem99@gmail.com>
2019-10-09 16:09:55 +0700

    createmodule6&7

---
+++ wp-content/themes/seagamefootball/sidebar-module6.php
@@ -0,0 +1,97 @@
+<?php $Url = get_template_directory_uri(); ?>
+<div class="container">
+				<!-- ROW -->
+				<div class="row">
+					<!-- Main Column -->
+					<div class="col-md-12">
+						<!-- section title -->
+						<div class="section-title">
+							<h2 class="title">Popular Video</h2>
+							<div id="nav-carousel-2" class="custom-owl-nav pull-right"><div class="owl-prev disabled"><i class="fa fa-angle-left"></i></div><div class="owl-next"><i class="fa fa-angle-right"></i></div></div>
+						</div>
+						<!-- /section title -->
+						
+						<!-- owl carousel 2 -->
+						<div id="owl-carousel-2" class="owl-carousel owl-theme owl-loaded owl-drag">
+							<!-- ARTICLE -->
+							
+							<!-- /ARTICLE -->
+							
+							<!-- ARTICLE -->
+							
+							<!-- /ARTICLE -->
+							
+							<!-- ARTICLE -->
+							
+							<!-- /ARTICLE -->
+							
+							<!-- ARTICLE -->
+							
+							<!-- /ARTICLE -->
+						<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all 0.25s ease 0s; width: 1540px;"><div class="owl-item active" style="width: 370px; margin-right: 15px;"><article class="article thumb-article">
+								<div class="article-img">
+									<img src="<?php echo $Url ?>/img/img-thumb-1.jpg" alt="">
+								</div>
+								<div class="article-body">
+									<ul class="article-info">
+										<li class="article-category"><a href="#">News</a></li>
+										<li class="article-type"><i class="fa fa-video-camera"></i></li>
+									</ul>
+									<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+									<ul class="article-meta">
+										<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+										<li><i class="fa fa-comments"></i> 33</li>
+									</ul>
+								</div>
+							</article></div><div class="owl-item active" style="width: 370px; margin-right: 15px;"><article class="article thumb-article">
+								<div class="article-img">
+									<img src="<?php echo $Url ?>/img/img-thumb-2.jpg" alt="">
+								</div>
+								<div class="article-body">
+									<ul class="article-info">
+										<li class="article-category"><a href="#">News</a></li>
+										<li class="article-type"><i class="fa fa-video-camera"></i></li>
+									</ul>
+									<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+									<ul class="article-meta">
+										<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+										<li><i class="fa fa-comments"></i> 33</li>
+									</ul>
+								</div>
+							</article></div><div class="owl-item active" style="width: 370px; margin-right: 15px;"><article class="article thumb-article">
+								<div class="article-img">
+									<img src="<?php echo $Url ?>/img/img-thumb-3.jpg" alt="">
+								</div>
+								<div class="article-body">
+									<ul class="article-info">
+										<li class="article-category"><a href="#">News</a></li>
+										<li class="article-type"><i class="fa fa-video-camera"></i></li>
+									</ul>
+									<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+									<ul class="article-meta">
+										<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+										<li><i class="fa fa-comments"></i> 33</li>
+									</ul>
+								</div>
+							</article></div><div class="owl-item" style="width: 370px; margin-right: 15px;"><article class="article thumb-article">
+								<div class="article-img">
+									<img src="<?php echo $Url ?>/img/img-thumb-4.jpg" alt="">
+								</div>
+								<div class="article-body">
+									<ul class="article-info">
+										<li class="article-category"><a href="#">News</a></li>
+										<li class="article-type"><i class="fa fa-video-camera"></i></li>
+									</ul>
+									<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+									<ul class="article-meta">
+										<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+										<li><i class="fa fa-comments"></i> 33</li>
+									</ul>
+								</div>
+							</article></div></div></div><div class="owl-dots disabled"></div></div>
+						<!-- /owl carousel 2 -->
+					</div>
+					<!-- /Main Column -->
+				</div>
+				<!-- /ROW -->
+			</div>
---
+++ wp-content/themes/seagamefootball/sidebar-module7.php
@@ -0,0 +1,107 @@
+<?php $Url = get_template_directory_uri(); ?>
+<div class="col-md-8">
+						<!-- section title -->
+						<div class="section-title">
+							<h2 class="title">Popular Posts</h2>
+						</div>
+						<!-- /section title -->
+						
+						<!-- ARTICLE -->
+						<article class="article row-article">
+							<div class="article-img">
+								<a href="#">
+									<img src="<?php echo $Url ?>/img/img-md-1.jpg" alt="">
+								</a>
+							</div>
+							<div class="article-body">
+								<ul class="article-info">
+									<li class="article-category"><a href="#">News</a></li>
+									<li class="article-type"><i class="fa fa-file-text"></i></li>
+								</ul>
+								<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+								<ul class="article-meta">
+									<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+									<li><i class="fa fa-comments"></i> 33</li>
+								</ul>
+								<p>Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis, ne alia sadipscing mei. Te inciderint cotidieque pro, ei iisque docendi qui.</p>
+							</div>
+						</article>
+						<!-- /ARTICLE -->
+						
+						<!-- ARTICLE -->
+						<article class="article row-article">
+							<div class="article-img">
+								<a href="#">
+									<img src="<?php echo $Url ?>/img/img-md-2.jpg" alt="">
+								</a>
+							</div>
+							<div class="article-body">
+								<ul class="article-info">
+									<li class="article-category"><a href="#">News</a></li>
+									<li class="article-type"><i class="fa fa-file-text"></i></li>
+								</ul>
+								<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+								<ul class="article-meta">
+									<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+									<li><i class="fa fa-comments"></i> 33</li>
+								</ul>
+								<p>Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis, ne alia sadipscing mei. Te inciderint cotidieque pro, ei iisque docendi qui.</p>
+							</div>
+						</article>
+						<!-- /ARTICLE -->
+						
+						<!-- ARTICLE -->
+						<article class="article row-article">
+							<div class="article-img">
+								<a href="#">
+									<img src="<?php echo $Url ?>/img/img-md-3.jpg" alt="">
+								</a>
+							</div>
+							<div class="article-body">
+								<ul class="article-info">
+									<li class="article-category"><a href="#">News</a></li>
+									<li class="article-type"><i class="fa fa-file-text"></i></li>
+								</ul>
+								<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+								<ul class="article-meta">
+									<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+									<li><i class="fa fa-comments"></i> 33</li>
+								</ul>
+								<p>Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis, ne alia sadipscing mei. Te inciderint cotidieque pro, ei iisque docendi qui.</p>
+							</div>
+						</article>
+						<!-- /ARTICLE -->
+						
+						<!-- ARTICLE -->
+						<article class="article row-article">
+							<div class="article-img">
+								<a href="#">
+									<img src="<?php echo $Url ?>/img/img-md-4.jpg" alt="">
+								</a>
+							</div>
+							<div class="article-body">
+								<ul class="article-info">
+									<li class="article-category"><a href="#">News</a></li>
+									<li class="article-type"><i class="fa fa-file-text"></i></li>
+								</ul>
+								<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+								<ul class="article-meta">
+									<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+									<li><i class="fa fa-comments"></i> 33</li>
+								</ul>
+								<p>Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis, ne alia sadipscing mei. Te inciderint cotidieque pro, ei iisque docendi qui.</p>
+							</div>
+						</article>
+						<!-- /ARTICLE -->
+						
+						<!-- pagination -->
+						<div class="article-pagination">
+							<ul> 
+								<li class="active"><a href="#" class="active">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+							</ul>
+						</div>
+						<!-- /pagination -->
+					</div>
