a8e166e722dabbfc3b5c83ed6abcc7925a43e801
doanvuquockhiem <Doanvukhiem99@gmail.com>
2019-10-10 07:39:21 +0700

    module7 ver1

--- wp-content/themes/seagamefootball/sidebar-module7.php
+++ wp-content/themes/seagamefootball/sidebar-module7.php
@@ -1,5 +1,9 @@
 <?php $Url = get_template_directory_uri(); ?>
-<div class="col-md-8">
+<div class="container">
+				<!-- ROW -->
+				<div class="row">
+					<!-- Main Column -->
+					<div class="col-md-8">
 						<!-- section title -->
 						<div class="section-title">
 							<h2 class="title">Popular Posts</h2>
@@ -105,3 +109,167 @@
 						</div>
 						<!-- /pagination -->
 					</div>
+					<!-- /Main Column -->
+					
+					<!-- Aside Column -->
+					<div class="col-md-4">
+						<!-- article widget -->
+						<div class="widget">
+							<div class="widget-title">
+								<h2 class="title">Featured Posts</h2>
+							</div>
+							
+							<!-- owl carousel 4 -->
+							<div id="owl-carousel-4" class="owl-carousel owl-theme owl-loaded owl-drag">
+								<!-- ARTICLE -->
+								
+								<!-- /ARTICLE -->
+							
+								<!-- ARTICLE -->
+								
+								<!-- /ARTICLE -->
+							<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-720px, 0px, 0px); transition: all 0.25s ease 0s; width: 2160px;"><div class="owl-item cloned" style="width: 360px;"><article class="article thumb-article">
+									<div class="article-img">
+										<img src="<?php echo $Url ?>/img/img-thumb-1.jpg" alt="">
+									</div>
+									<div class="article-body">
+										<ul class="article-info">
+											<li class="article-category"><a href="#">News</a></li>
+											<li class="article-type"><i class="fa fa-video-camera"></i></li>
+										</ul>
+										<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+										<ul class="article-meta">
+											<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+											<li><i class="fa fa-comments"></i> 33</li>
+										</ul>
+									</div>
+								</article></div><div class="owl-item cloned" style="width: 360px;"><article class="article thumb-article">
+									<div class="article-img">
+										<img src="<?php echo $Url ?>/img/img-thumb-2.jpg" alt="">
+									</div>
+									<div class="article-body">
+										<ul class="article-info">
+											<li class="article-category"><a href="#">News</a></li>
+											<li class="article-type"><i class="fa fa-video-camera"></i></li>
+										</ul>
+										<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+										<ul class="article-meta">
+											<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+											<li><i class="fa fa-comments"></i> 33</li>
+										</ul>
+									</div>
+								</article></div><div class="owl-item active" style="width: 360px;"><article class="article thumb-article">
+									<div class="article-img">
+										<img src="<?php echo $Url ?>/img/img-thumb-1.jpg" alt="">
+									</div>
+									<div class="article-body">
+										<ul class="article-info">
+											<li class="article-category"><a href="#">News</a></li>
+											<li class="article-type"><i class="fa fa-video-camera"></i></li>
+										</ul>
+										<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+										<ul class="article-meta">
+											<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+											<li><i class="fa fa-comments"></i> 33</li>
+										</ul>
+									</div>
+								</article></div><div class="owl-item" style="width: 360px;"><article class="article thumb-article">
+									<div class="article-img">
+										<img src="<?php echo $Url ?>/img/img-thumb-2.jpg" alt="">
+									</div>
+									<div class="article-body">
+										<ul class="article-info">
+											<li class="article-category"><a href="#">News</a></li>
+											<li class="article-type"><i class="fa fa-video-camera"></i></li>
+										</ul>
+										<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+										<ul class="article-meta">
+											<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+											<li><i class="fa fa-comments"></i> 33</li>
+										</ul>
+									</div>
+								</article></div><div class="owl-item cloned" style="width: 360px;"><article class="article thumb-article">
+									<div class="article-img">
+										<img src="<?php echo $Url ?>/img/img-thumb-1.jpg" alt="">
+									</div>
+									<div class="article-body">
+										<ul class="article-info">
+											<li class="article-category"><a href="#">News</a></li>
+											<li class="article-type"><i class="fa fa-video-camera"></i></li>
+										</ul>
+										<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+										<ul class="article-meta">
+											<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+											<li><i class="fa fa-comments"></i> 33</li>
+										</ul>
+									</div>
+								</article></div><div class="owl-item cloned" style="width: 360px;"><article class="article thumb-article">
+									<div class="article-img">
+										<img src="<?php echo $Url ?>/img/img-thumb-2.jpg" alt="">
+									</div>
+									<div class="article-body">
+										<ul class="article-info">
+											<li class="article-category"><a href="#">News</a></li>
+											<li class="article-type"><i class="fa fa-video-camera"></i></li>
+										</ul>
+										<h3 class="article-title"><a href="#">Duis urbanitas eam in, tempor consequat.</a></h3>
+										<ul class="article-meta">
+											<li><i class="fa fa-clock-o"></i> January 31, 2017</li>
+											<li><i class="fa fa-comments"></i> 33</li>
+										</ul>
+									</div>
+								</article></div></div></div><div class="owl-nav disabled"><div class="owl-prev">prev</div><div class="owl-next">next</div></div><div class="owl-dots"><div class="owl-dot active"><span></span></div><div class="owl-dot"><span></span></div></div></div>
+							<!-- /owl carousel 4 -->
+						</div>
+						<!-- /article widget -->
+						
+						<!-- galery widget -->
+						<div class="widget galery-widget">
+							<div class="widget-title">
+								<h2 class="title">Flickr Photos</h2>
+							</div>
+							<ul>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-3.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-4.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-5.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-6.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-7.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-8.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-9.jpg" alt=""></a></li>
+								<li><a href="#"><img src="<?php echo $Url ?>/img/img-widget-10.jpg" alt=""></a></li>
+							</ul>
+						</div>
+						<!-- /galery widget -->
+						
+						<!-- tweets widget -->
+						<div class="widget tweets-widget">
+							<div class="widget-title">
+								<h2 class="title">Recent Tweets</h2>
+							</div>
+							<ul>
+								<li class="tweet">
+									<i class="fa fa-twitter"></i>
+									<div class="tweet-body">
+										<p><a href="#">@magnews</a> Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis <a href="#">https://t.co/DwsTbsmxTP</a></p>
+									</div>
+								</li>
+								<li class="tweet">
+									<i class="fa fa-twitter"></i>
+									<div class="tweet-body">
+										<p><a href="#">@magnews</a> Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis <a href="#">https://t.co/DwsTbsmxTP</a></p>
+									</div>
+								</li>
+								<li class="tweet">
+									<i class="fa fa-twitter"></i>
+									<div class="tweet-body">
+										<p><a href="#">@magnews</a> Populo tritani laboramus ex mei, no eum iuvaret ceteros euripidis <a href="#">https://t.co/DwsTbsmxTP</a></p>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<!-- /tweets widget -->
+					</div>
+					<!-- /Aside Column -->
+				</div>
+				<!-- /ROW -->
+			</div>
