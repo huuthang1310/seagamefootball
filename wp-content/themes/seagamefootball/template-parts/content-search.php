75f060207e07365a213e08c1e2387ea50b6077d8
khangnguyen33999@gmail.com <kanewtf@gmail.com>
2019-10-14 15:01:14 +0700

    search page

--- wp-content/themes/seagamefootball/template-parts/content-search.php
+++ wp-content/themes/seagamefootball/template-parts/content-search.php
@@ -10,11 +10,49 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		 <a href="<?php the_permalink();?>">
-                                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-		
+	<div class="section">
+    <!-- CONTAINER -->
+    <div class="container">
+        <!-- ROW -->
+        <div class="row">
+            <!-- Main Column -->
+            <div class="col-md-12">
+                <!-- section title -->
+                <div class="section-title">
+                  
+                </div>
+                <!-- /section title -->
+
+                <!-- ARTICLE -->
+
+                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> <?php the_time('F jS, Y'); ?></li>
+                            <li><i class="fa fa-comments"></i> <?php echo get_comments_number();?></li>
+                        </ul>
+                        <?php echo $rest = substr(get_the_excerpt(), 0, 400 ); ?>
+                        <a href="<?php the_permalink(); ?>"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+              
+            </div>
+
+        </div>
+        <!-- /ROW -->
+    </div>
+    <!-- /CONTAINER -->
+</div>
 	</header><!-- .entry-header -->
 
 	
