7ebac95fe3ed758b22976174a21df188cf6066c3
huuthang1310 <buihuuthang199@gmail.com>
2019-10-14 13:39:35 +0700

    fix bug

--- wp-content/themes/seagamefootball/sidebar-module1.php
+++ wp-content/themes/seagamefootball/sidebar-module1.php
@@ -1,7 +1,7 @@
 <?php $Url = get_template_directory_uri(); ?>
 <header id="header">
     <!-- Top Header -->
-    
+
     <!-- /Top Header -->
 
     <!-- Center Header -->
@@ -9,12 +9,10 @@
         <div class="container">
             <div class="header-logo">
                 <a href="#" class="logo"><img src="<?php echo $Url ?>/img/images1.png" alt=""></a>
+
             </div>
             <div class="header-ads">
-            <div class="adv" id="ads_1" style="margin-top:0px;margin-bottom:0px;"><div class="e" style=""> <!-- /424536528/1399789_bongda.com.vn_PC_Masthead -->
-                            <div id="div-gpt-ad-1548311393614-0" data-google-query-id="CNCg4JnpleUCFc6HaAodUZgBGQ">
-                            
-                            <div id="google_ads_iframe_/424536528/1399789_bongda.com.vn_PC_Masthead_0__container__" style="border: 0pt none; display: inline-block; width: 970px; height: 90px;"><iframe frameborder="0" src="https://tpc.googlesyndication.com/safeframe/1-0-35/html/container.html" id="google_ads_iframe_/424536528/1399789_bongda.com.vn_PC_Masthead_0" title="3rd party ad content" name="" scrolling="no" marginwidth="0" marginheight="0" width="970" height="90" data-is-safeframe="true" sandbox="allow-forms allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" data-google-container-id="1" style="border: 0px; vertical-align: bottom;" data-load-complete="true"></iframe></div></div></div></div>
+                <img class="center-block" src="<?php echo $Url ?>/img/banner1.png" alt="">
             </div>
         </div>
     </div>
