7c464ed029c4355a37891524f113cec87baeca9f
doanvuquockhiem <Doanvukhiem99@gmail.com>
2019-10-17 10:53:21 +0700

    update-page404

--- wp-content/themes/seagamefootball/404.php
+++ wp-content/themes/seagamefootball/404.php
@@ -9,52 +9,71 @@
 
 get_header();
 ?>
+<style>
+
+	.s1 {
+    border-bottom: 0px solid #e1e1e1;}
+    .inner{
+    	padding: 10px;
+    }
+    
+  	.backBtn{
+    display: inline-block;
+    font-size: 15px;
+    color: #fff;
+    padding: 10px 30px;
+    padding-left: 50px;
+    font-weight: bold;
+    background: url(//cdn.24h.com.vn/images/Forma-1-copy-home.png) no-repeat 20px;
+    background-color: #e31010;
+}
+.page-header {
+    padding-bottom: 9px;
+    margin: 40px 0 20px;
+    border-bottom: 100px solid #fff;
+}
+@media screen and (max-width: 480px){
+				    
+}
+</style>
+
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'seagamefootball' ); ?></h1>
-				</header><!-- .page-header -->
+					<h1 class="page-title" style="text-align: center;"><?php esc_html_e( 'Truy cập của bạn có thể bị lỗi hoặc không tìm thấy nội dung.', 'seagamefootball' ); ?></h1>
+					<div class="page-content">
+					<div class="s1">
+						<div style="width: 50%;margin: 50px auto;">
+							<div class="img">
+								<img src="https://cdn.24h.com.vn//images/404img_092018.png" alt="">
+							</div>
+							<div class="inner">
+								<div class="head">
+								<a href="<?php echo get_site_url() ?>" class="backBtn">Quay lại trang chủ</a>
+								</div>
+							</div>
+							<div class="clear"></div>
+						</div>
+					</div>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'seagamefootball' ); ?></p>
+					
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'seagamefootball' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$seagamefootball_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'seagamefootball' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$seagamefootball_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 				</div><!-- .page-content -->
+				</header><!-- .page-header -->
+
+				
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+	
 
 <?php
 get_footer();
