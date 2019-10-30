35e29e19b26d2213411d297a5c634bc6a2313ab9
doanvuquockhiem <Doanvukhiem99@gmail.com>
2019-10-30 14:21:36 +0700

    up-module-11-12

--- wp-content/themes/seagamefootball/module/module-12/12-content.php
+++ wp-content/themes/seagamefootball/module/module-12/12-content.php
@@ -1,44 +1,62 @@
-<!-- Thông Tin Nhóm -->
-<div id="myModal1" class="modal fade in" role="dialog" style="display: block; padding-right: 17px;">
-            <div class="modal-dialog">
+<!-- 404-error -->
 
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">×</button>
-                        <h4 class="modal-title">Thông Tin Nhóm</h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <p>Nhóm B Chuyên Đề Phát Triển Web 2</p>
-                                <p> + Bùi Hửu Thắng</p>
-                                <p> + Nguyễn Hãi Phúc Khang</p>
-                                <p> + Nguyễn Minh Trinh</p>
-                                <p> + Đoàn Vũ Quốc Khiêm</p>
+<style>
+
+    .s1 {
+    border-bottom: 0px solid #e1e1e1;}
+    .inner{
+        padding: 10px;
+    }
+    
+    .backBtn{
+    display: inline-block;
+    font-size: 15px;
+    color: #fff;
+    padding: 10px 30px;
+    padding-left: 50px;
+    font-weight: bold;
+    background: url(//cdn.24h.com.vn/images/Forma-1-copy-home.png) no-repeat 20px;
+    background-color: #e31010;
+}
+.page-header {
+    padding-bottom: 9px;
+    margin: 40px 0 20px;
+    border-bottom: 100px solid #fff;
+}
+@media screen and (max-width: 480px){
+                    
+}
+</style>
+<div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <section class="error-404 not-found">
+                <header class="page-header">
+                    <h1 class="page-title" style="text-align: center;">Truy cập của bạn có thể bị lỗi hoặc không tìm thấy nội dung.</h1>
+                    <div class="page-content">
+                    <div class="s1">
+                        <div style="width: 100%;">
+                            <div class="img" style="text-align: center;">
+                                <img src="https://cdn.24h.com.vn//images/404img_092018.png" alt="">
                             </div>
-                            <div class="col-md-6">
-                                <p>Giáo Viên Hướng Dẫn
-                                </p>
-                                <p>+ Thầy Phan Thanh Nhuần</p>
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <img style="width : 70px;" class="img-responsive" data-src="http://localhost/seagamefootball/wp-content/themes/seagamefootball/img/Logo-Cao-dang1.jpg" src="http://localhost/seagamefootball/wp-content/themes/seagamefootball/img/Logo-Cao-dang1.jpg">
-                                    </div>
-                                    <div class="col-md-8">
-                                        <p>Trường Cao Đẳng Công Nghệ Thủ Đức</p>
-                                        <p>Địa chỉ : 53 Võ Văn Ngân, Linh Chiểu, Thủ Đức, Hồ Chí Minh</p>
-                                    </div>
+                            <div class="inner">
+                                <div class="head" style="text-align: center;">
+                                <a href="http://localhost/seagamefootball" class="backBtn">Quay lại trang chủ</a>
                                 </div>
                             </div>
-
+                            <div class="clear"></div>
                         </div>
-
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
 
-            </div>
-        </div>
+                    
+
+                    
+
+                </div><!-- .page-content -->
+                </header><!-- .page-header -->
+
+                
+            </section><!-- .error-404 -->
+
+        </main><!-- #main -->
+    </div>
