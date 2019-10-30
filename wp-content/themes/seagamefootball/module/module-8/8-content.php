82cd9f203680cee826fc76c24fc794ef5319a55e
Nguyenminhtrinh <nguyenminhtrinhyuu@gmail.com>
2019-10-30 15:41:57 +0700

    module789

---
+++ wp-content/themes/seagamefootball/module/module-8/8-content.php
@@ -0,0 +1,196 @@
+<!-- Tinh tuc Tong Hop -->
+<section id = "module-8">
+<div class="section">
+    <!-- CONTAINER -->
+    <div class="container">
+        <!-- ROW -->
+        <div class="row">
+            <!-- Main Column -->
+            <div class="col-md-12">
+                <!-- section title -->
+                <div class="section-title">
+                    <h2 class="title">Tin Tổng Hợp</h2>
+                </div>
+                <!-- /section title -->
+
+                <!-- ARTICLE -->
+                
+                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/photo-1-15678428446541222848734.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/bao-trung-quoc-gap-viet-nam-la-co-hoi-de-hoc-tap/">Báo Trung Quốc: Găp Việt Nam là cơ hội để học tập</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Truyền thông Trung Quốc tỏ ra khá thận trọng khi nhận định về trận giao hữu gặp U22 Việt Nam. Để chuẩn bị cho SEA Games 30 sắp diễn ra vào cuối năm, LĐBĐ Việt Nam đã lên kế hoạch cho trận đấu giao hữa giữa&nbsp;U22 Việt Nam&nbsp;vs U22 Trung Quốc. HLV Park Hang Seo […]                        <a href="http://localhost/seagamefootball/bao-trung-quoc-gap-viet-nam-la-co-hoi-de-hoc-tap/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/photo1567989938497-1567989938698-crop-15679900749691455966057.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/u22-viet-nam-ha-dep-u22-trung-quoc-bac-thay-tro-choi-park-hang-seo/">U22 Việt Nam hạ đẹp U22 Trung Quốc: Bậc thầy trò chơi, Park Hang Seo!</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        U22 Việt Nam thắng đẹp U22 Trung Quốc trong ngày HLV Park Hang Seo đối đầu&nbsp;‘phù thủy’ Hiddink, với câu trả lời ai mới là bậc thầy trò chơi lúc này Trước khi có chiến thắng đẹp ở trận giao hữu với U22 Trung Quốc, rõ ràng U22&nbsp;Việt Nam&nbsp;không được quan tâm quá nhiều, thậm […]                        <a href="http://localhost/seagamefootball/u22-viet-nam-ha-dep-u22-trung-quoc-bac-thay-tro-choi-park-hang-seo/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/photo-1-15679895858821967460184-crop-156798962341584655929.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/cdv-chau-a-chi-vai-nam-nua-viet-nam-se-duoc-du-world-cup/">CĐV châu Á: ‘Chỉ vài năm nữa, Việt Nam sẽ được dự World Cup’</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Chiến thắng của đội U22 Việt Nam trước U22 Trung Quốc đã giúp cho thầy trò HLV Park Hang Seo nhận được vô số lời ca ngợi. U22&nbsp;Việt Nam&nbsp;vừa có chiến thắng 2-0 trước U22 Trung Quốc trong trận đấu giao hữu diễn ra ngày 8/9. Cả 2 bàn thắng bên phía đội U22 Việt […]                        <a href="http://localhost/seagamefootball/cdv-chau-a-chi-vai-nam-nua-viet-nam-se-duoc-du-world-cup/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/6983609223246830345106951715042374721208320o-1568012474700826342719-crop-1568012480997327829337.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/u22-viet-nam-neu-them-quang-hai-van-hau-dinh-trong-se-du-kha-nang-vo-dich-sea-games-30/">“U22 Việt Nam nếu thêm Quang Hải, Văn Hậu, Đình Trọng sẽ đủ khả năng vô địch SEA Games 30”</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Đó là nhận định của HLV Phạm Minh Đức về lứa U22 Việt Nam vừa đánh bại U22 Trung Quốc 2-0 trong trận đấu tối qua. Ở trận giao hữu quốc tế đầu tiên trong hành trình chuẩn bị cho SEA Games 30, U22&nbsp;Việt Nam&nbsp;đã có được chiến thắng dễ dàng 2-0 trước U22 Trung […]                        <a href="http://localhost/seagamefootball/u22-viet-nam-neu-them-quang-hai-van-hau-dinh-trong-se-du-kha-nang-vo-dich-sea-games-30/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/photo1567953706083-1567953706268-crop-15679537113621986066574.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/u22-viet-nam-thang-trung-quoc-gia-tri-cua-mot-cuoc-tap-tran/">U22 Việt Nam thắng Trung Quốc: Giá trị của một cuộc ‘tập trận’</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Chiến thắng trước đội tuyển U22 Trung Quốc, dù chỉ ở một trân đấu giao hữu nhưng đã cho thấy những tín hiệu tích cực với bóng đá Việt Nam. Nhiều người đã cảm thấy bất ngờ khi HLV Park Hang Seo đưa Tiến Linh vào danh sách đá chính ở trận đấu với đội […]                        <a href="http://localhost/seagamefootball/u22-viet-nam-thang-trung-quoc-gia-tri-cua-mot-cuoc-tap-tran/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/nu5-1567862971855933113709-crop-15678629767071038915607.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/quan-bau-duc-lam-doi-truong-u22-viet-nam-tran-dau-voi-u22-trung-quoc/">Quân bầu Đức làm đội trưởng U22 Việt Nam trận đấu với U22 Trung Quốc</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Tiền vệ Triệu Việt Hưng của Hoàng Anh Gia Lai sẽ mang băng đội trưởng U22 Việt Nam trong trận giao hữu quốc tế với U22 Trung Quốc. Triệu Việt Hưng&nbsp;từng là mắt xích quan trọng của U23 Việt nam tại vòng loại U23 châu Á diễn ra trên sân Mỹ Đình. Tiền vệ trung […]                        <a href="http://localhost/seagamefootball/quan-bau-duc-lam-doi-truong-u22-viet-nam-tran-dau-voi-u22-trung-quoc/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/1-1570151353609421842064-crop-15701513680481993775371.png" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/dieu-chinh-ke-hoach-thi-dau-cua-u22-viet-nam/">Điều chỉnh kế hoạch thi đấu của U22 Việt Nam</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Liên đoàn Bóng đá Việt Nam sẽ huỷ lịch thi đấu của đội tuyển U22 Việt Nam tại giải BTV Cup ở Bình Dương. Theo lịch trình dự kiến, với mục tiêu thử nghiệm đội hình và rà soát lực lượng để chuẩn bị cho SEA Games 30 tại Philippines vào tháng 11 tới, đội […]                        <a href="http://localhost/seagamefootball/dieu-chinh-ke-hoach-thi-dau-cua-u22-viet-nam/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/ducchinh-1570990681837893613871-crop-15709906892671976851083.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/thu-nghiem-nhieu-vi-tri-song-u22-viet-nam-van-co-mot-tran-dau-kha-tot-truoc-u22-uae/">Thử nghiệm nhiều vị trí, song U22 Việt Nam vẫn có một trận đấu khá tốt trước U22 UAE.</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Mặc dù còn những thời điểm lỗi nhịp, song nhìn chung&nbsp;U22 Việt Nam&nbsp;thi đấu khá tự tin trước U22 UAE. Đoàn quân áo đỏ tạo ra những tình huống sóng gió trước khung thành đối thủ Tây Á và kết thúc trận đấu bằng tỉ số hòa 1-1. Tờ FOX Sports Asia dành lời khen […]                        <a href="http://localhost/seagamefootball/thu-nghiem-nhieu-vi-tri-song-u22-viet-nam-van-co-mot-tran-dau-kha-tot-truoc-u22-uae/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                <article class="article row-article">
+                    <div class="article-img">
+                        <a href="#">
+                            <img src="http://localhost/seagamefootball/wp-content/uploads/2019/10/sg-15711126149621016360813-crop-1571112632108494839302.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="article-body">
+                        <ul class="article-info">
+                            <li class="article-category"><a href="#">Tin Tức</a></li>
+                            <li class="article-type"><i class="fa fa-file-text"></i></li>
+                        </ul>
+                        <h3 class="article-title"><a href="http://localhost/seagamefootball/viet-nam-dung-thai-lan-o-bang-tu-than-cdv-dong-nam-a-nghi-ngo-chu-nha-sap-xep-de-vao-bang-de-tai-sea-games-30/">Việt Nam đụng Thái Lan ở bảng tử thần, CĐV Đông Nam Á nghi ngờ chủ nhà</a></h3>
+                        <ul class="article-meta">
+                            <li><i class="fa fa-clock-o"></i> October 16th, 2019</li>
+                            <li><i class="fa fa-comments"></i> 0</li>
+                        </ul>
+                        Sau khi có kết quả bốc thăm vòng bảng môn bóng đá nam SEA Games 30, các cổ&nbsp; Lễ bốc thăm vòng bảng môn bóng đá nam&nbsp;SEA Games 30&nbsp;vào sáng nay 15/10 tại khách sạn Sofitel Philippine Plaza tại thủ đô Manila, Philippines. Kết quả bốc thăm thăm như sau: Kết quả bốc thăm vòng […]                        <a href="http://localhost/seagamefootball/viet-nam-dung-thai-lan-o-bang-tu-than-cdv-dong-nam-a-nghi-ngo-chu-nha-sap-xep-de-vao-bang-de-tai-sea-games-30/"><b>Xem thêm</b></a>
+                    </div>
+                </article>
+                                                                            </div>
+
+        </div>
+        <!-- /ROW -->
+    </div>
+    <!-- /CONTAINER -->
+</div>
+</section>
